04c4d2d5cc6bb0f3ecc5d15eb9ccfaac6260b246
Andrew Bayer <andrewbayer21@gmail.com>
2023-01-04 07:22:02 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            var userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+                case "English":
+                    Console.WriteLine("I like English too!");
+                    break;
+                case "Math":
+                    Console.WriteLine("Math is hard");
+                    break ;
+                case "Science":
+                    Console.WriteLine("Scicence is a good one.");
+                    break;
+                case "History":
+                    Console.WriteLine("History is boring");
+                    break;
+                case "Language Arts":
+                    Console.WriteLine("I've never understood the differnece between that and English");
+                  break;
+                default:
+                    Console.WriteLine("I've never heard of that one!");
+                    break;
+
+            }
         }
     }
 }
